e128750587368f40924d93e79e9fe9552ebb1dec
Taranpal <taranarora1470@gmail.com>
2025-07-30 19:30:15 +0530

    Basic setup done

---
+++ Frontend/f/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Signup from "./components/Signup";
+
+function app (){
+    return (
+        <>
+        <Signup/>
+        </>
+    )
+}
+export default app;
---
+++ Frontend/f/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup(){
+    return(
+        <>
+        This is my signup comoponent
+        </>
+    )
+}
+
+export default Signup
